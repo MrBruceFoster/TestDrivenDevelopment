b25536758204357875957097b5253552a5a1b948
Malte <malte.github@runbox.com>
2025-04-01 14:32:35 +0200

    Fizzbuzz done

---
+++ src/fizzbuzz/Main.java
@@ -0,0 +1,24 @@
+package fizzbuzz;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+    public String fizzbuzz(int i) {
+        String returnString;
+        if ((i % 15) == 0){
+            returnString = "fizzbuzz";
+        }
+        else if ((i % 3) == 0){
+            returnString = "fizz";
+        }
+        else if ((i % 5) == 0) {
+            returnString = "buzz";
+        }
+        else {
+            returnString = String.valueOf(i);
+        }
+        return returnString;
+    }
+}
